aa73659a52eaeeefe58e9df75fc1984d2ada414d
AnYoungjin <yjanmin@naver.com>
2021-12-08 13:59:21 +0900

    feat : add help-command

---
+++ server/command.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/mattermost/mattermost-plugin-api/experimental/command"
+	"github.com/mattermost/mattermost-server/v6/model"
+	"github.com/mattermost/mattermost-server/v6/plugin"
+	"github.com/pkg/errors"
+)
+
+func (p *Plugin) getCommand() (*model.Command, error) {
+	iconData, err := command.GetIconData(p.API, "assets/mbotc-icon.svg")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get icon data")
+	}
+
+	return &model.Command{
+		Trigger:              "mbotc",
+		DisplayName:          "mbotc",
+		Description:          "Integration with MBotC.",
+		AutoComplete:         true,
+		AutoCompleteDesc:     "Available commands: help",
+		AutoCompleteHint:     "[command]",
+		AutocompleteData:     getAutocompleteData(),
+		AutocompleteIconData: iconData,
+	}, nil
+}
+
+const helpText = "###### Mattermost MBotC Plugin - Slash Command Help\n" +
+	"* `/mbotc help` - help text"
+
+type CommandHandlerFunc func(p *Plugin, c *plugin.Context, header *model.CommandArgs, args ...string) *model.CommandResponse
+
+type CommandHandler struct {
+	handlers       map[string]CommandHandlerFunc
+	defaultHandler CommandHandlerFunc
+}
+
+//===================================================
+// command Handler
+// command : func
+//===================================================
+var mbotcCommandHandler = CommandHandler{
+	handlers: map[string]CommandHandlerFunc{
+		"help":       executeHelp,
+	},
+	defaultHandler: executeHelp,
+}
+
+func (ch CommandHandler) Handle(p *Plugin, c *plugin.Context, header *model.CommandArgs, args ...string) *model.CommandResponse {
+	for n := len(args); n > 0; n-- {
+		h := ch.handlers[strings.Join(args[:n], "/")]
+		if h != nil {
+			return h(p, c, header, args[n:]...)
+		}
+	}
+	return ch.defaultHandler(p, c, header, args...)
+}
+
+func executeHelp(p *Plugin, c *plugin.Context, header *model.CommandArgs, args ...string) *model.CommandResponse {
+	return p.help(header)
+}
+
+func (p *Plugin) help(header *model.CommandArgs) *model.CommandResponse {
+	p.postCommandResponse(header, helpText)
+	return &model.CommandResponse{}
+}
+
+func (p *Plugin) ExecuteCommand(c *plugin.Context, commandArgs *model.CommandArgs) (*model.CommandResponse, *model.AppError) {
+	args := strings.Fields(commandArgs.Command)
+	if len(args) == 0 || args[0] != "/mbotc" {
+		return p.help(commandArgs), nil
+	}
+
+	return mbotcCommandHandler.Handle(p, c, commandArgs, args[1:]...), nil
+}
+
+func getAutocompleteData() *model.AutocompleteData {
+	webexAutocomplete := model.NewAutocompleteData("mbotc", "[command]", "Available commands: help")
+
+	help := model.NewAutocompleteData("help", "", "Guide for mbotc")
+	webexAutocomplete.AddCommand(help)
+
+	return webexAutocomplete
+}
+
+// Post Message to Channel
+func (p *Plugin) postCommandResponse(args *model.CommandArgs, text string) {
+	post := &model.Post{
+		UserId:    p.botUserID,
+		ChannelId: args.ChannelId,
+		Message:   text,
+	}
+	_ = p.API.SendEphemeralPost(args.UserId, post)
+}
--- server/plugin.go
+++ server/plugin.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"net/http"
 	"sync"
 
+	"github.com/mattermost/mattermost-server/v6/model"
 	"github.com/mattermost/mattermost-server/v6/plugin"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	botUserName    = "mbotc"
+	botDisplayName = "MBotC"
+	botDescription = "Created by the MBotC plugin."
 )
 
 // Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
 type Plugin struct {
 	plugin.MattermostPlugin
+
+	// botUserID of the created bot account.
+	botUserID string
+	bot *model.Bot
 
 	// configurationLock synchronizes access to the configuration.
 	configurationLock sync.RWMutex
@@ -18,6 +33,59 @@ type Plugin struct {
 	// configuration is the active plugin configuration. Consult getConfiguration and
 	// setConfiguration for usage.
 	configuration *configuration
+
+	// KV store
+	store Store
+}
+
+// OnActivate is invoked when the plugin is activated. If an error is returned, the plugin
+// will be terminated. The plugin will not receive hooks until after OnActivate returns
+// without error. OnConfigurationChange will be called once before OnActivate.
+// OnActivate checks if the configurations is valid and ensures the bot account exists
+func (p *Plugin) OnActivate() error {
+	// create a bot
+	bot, appErr := p.API.CreateBot(&model.Bot{
+		Username:    botUserName,
+		DisplayName: botDisplayName,
+		Description: botDescription,
+	})
+	if appErr != nil {
+		return errors.Wrap(appErr, "failed to create bot account")
+	}
+	// allocate botUserID if success to create bot account
+	p.botUserID = bot.UserId
+
+	// GetBundlePath returns the absolute path where the plugin's bundle was unpacked.
+	bundlePath, err := p.API.GetBundlePath()
+	if err != nil {
+		return errors.Wrap(err, "couldn't get bundle path")
+	}
+
+	// get bundlePath/assets/profile.png
+	profileImage, err := ioutil.ReadFile(filepath.Join(bundlePath, "assets", "profile.png"))
+	if err != nil {
+		return errors.Wrap(err, "couldn't read profile image")
+	}
+
+	// Set bot profile image
+	if appErr := p.API.SetProfileImage(bot.UserId, profileImage); appErr != nil {
+		return errors.Wrap(appErr, "couldn't set profile image")
+	}
+
+	p.store = NewStore(p)
+
+	// getCommand() of command.go
+	command, err := p.getCommand()
+	if err != nil {
+		return errors.Wrap(err, "failed to get command")
+	}
+
+	err = p.API.RegisterCommand(command)
+	if err != nil {
+		return errors.WithMessage(err, "OnActivate: failed to register command")
+	}
+
+	return nil
 }
 
 // ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
