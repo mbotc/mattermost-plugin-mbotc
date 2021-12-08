9cd7dc0fa15d2645f33ce9ebe60d36f4b6fc0f50
Xayahx <guriguri8833@gmail.com>
2021-12-08 15:45:00 +0900

    feat : add today command

--- server/command.go
+++ server/command.go
@@ -32,6 +32,15 @@ func (p *Plugin) getCommand() (*model.Command, error) {
 	}, nil
 }
 
+type DailyNotice struct {
+	ChannelName string `json:"channel_name"`
+	EndTime     string `json:"end_time"`
+	Message     string `json:"message"`
+	StartTime   string `json:"start_time"`
+	TeamName    string `json:"team_name"`
+	UserName    string `json:"user_name"`
+}
+
 const helpText = "###### Mattermost MBotC Plugin - Slash Command Help\n" +
 	"* `/mbotc help` - help text\n" +
 	"* `/mbotc term` - Register your Term Notice.\n" +
@@ -60,9 +69,10 @@ type CommandHandler struct {
 //===================================================
 var mbotcCommandHandler = CommandHandler{
 	handlers: map[string]CommandHandlerFunc{
-		"help": executeHelp,
-		"term": executeTerm,
-		"date": executeDate,
+		"help":  executeHelp,
+		"term":  executeTerm,
+		"date":  executeDate,
+		"today": executeToday,
 	},
 	defaultHandler: executeHelp,
 }
@@ -96,6 +106,52 @@ func executeDate(p *Plugin, c *plugin.Context, header *model.CommandArgs, args .
 	p.postCommandResponse(header, "###### Successfully registered your term notice:\n"+header.Command)
 	p.registerNotice(header)
 	return &model.CommandResponse{}
+}
+
+func executeToday(p *Plugin, c *plugin.Context, header *model.CommandArgs, args ...string) *model.CommandResponse {
+	getNoticeList(p, header)
+	return &model.CommandResponse{}
+}
+
+func getNoticeList(p *Plugin, commandArgs *model.CommandArgs) {
+	// create new request
+	req, err := http.NewRequest("GET", "http://localhost:8080/api/v1/notification/today", nil)
+	if err != nil {
+		// panic()함수는 현재 함수를 즉시 멈추고 현재 함수에 defer 함수들을 모두 실행한 후 즉시 리턴한다
+		fmt.Println(err)
+		panic(err)
+	}
+
+	// set the header
+	req.Header.Add("auth", commandArgs.UserId)
+
+	client := &http.Client{}
+	resp, err := client.Do(req) // send request
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bytes, _ := ioutil.ReadAll(resp.Body)
+
+	var dailyNotices []DailyNotice
+	json.Unmarshal(bytes, &dailyNotices)
+
+	post := &model.Post{
+		UserId:    p.botUserID,
+		ChannelId: commandArgs.ChannelId,
+	}
+
+	for _, dn := range dailyNotices {
+		attachment, err := createAttachment(dn)
+		if err != nil {
+			fmt.Print(err)
+		}
+		post.AddProp("attachments", attachment)
+	}
+
+	_ = p.API.SendEphemeralPost(commandArgs.UserId, post)
 }
 
 // Send Post Request to our Bot server
@@ -138,6 +194,9 @@ func getAutocompleteData() *model.AutocompleteData {
 	date := model.NewAutocompleteData("date", "[text]", "Register your Date Notice (Please refer to /help)")
 	mbotcAutocomplete.AddCommand(date)
 
+	today := model.NewAutocompleteData("today", "", "Get all today's notices")
+	mbotcAutocomplete.AddCommand(today)
+
 	return mbotcAutocomplete
 }
 
@@ -149,4 +208,44 @@ func (p *Plugin) postCommandResponse(args *model.CommandArgs, text string) {
 		Message:   text,
 	}
 	_ = p.API.SendEphemeralPost(args.UserId, post)
+}
+
+func createAttachment(notice DailyNotice) ([]*model.SlackAttachment, error) {
+	var text = notice.Message
+	var fields []*model.SlackAttachmentField
+	var postedBy = notice.TeamName + " / " + notice.ChannelName
+
+	if notice.StartTime == notice.EndTime {
+		fields = append(fields, &model.SlackAttachmentField{
+			Title: ":calendar: Deadline",
+			Value: notice.StartTime,
+			Short: false,
+		})
+	} else {
+		fields = append(fields, &model.SlackAttachmentField{
+			Title: ":calendar: Start Time",
+			Value: notice.StartTime,
+			Short: true,
+		})
+		fields = append(fields, &model.SlackAttachmentField{
+			Title: ":calendar: End Time",
+			Value: notice.EndTime,
+			Short: true,
+		})
+	}
+
+	fields = append(fields, &model.SlackAttachmentField{
+		Title: ":lower_left_fountain_pen: Author",
+		Value: notice.UserName,
+		Short: false,
+	})
+
+	return []*model.SlackAttachment{
+		{
+			AuthorName: postedBy,
+			Color:      "#1352ab",
+			Text:       text,
+			Fields:     fields,
+		},
+	}, nil
 }
